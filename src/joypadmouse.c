cb04f32720382275d598c7f87857056028207318
Nicolas Albert <nicolas.albert@gmail.com>
2025-12-30 09:05:01 +0100

    Invert mouse axis direction

--- src/joypadmouse.c
+++ src/joypadmouse.c
@@ -848,6 +848,9 @@ int main(int argc, char **argv) {
       double ly = axis_ly < (int) (sizeof(axes) / sizeof(axes[0])) ? norm_axis(axes[axis_ly], deadzone) : 0.0;
       double ry = axis_ry < (int) (sizeof(axes) / sizeof(axes[0])) ? norm_axis(axes[axis_ry], deadzone) : 0.0;
 
+      lx = -lx;
+      ly = -ly;
+
       dx_acc += lx * (double) speed * mult / (double) poll_hz;
       dy_acc += ly * (double) speed * mult / (double) poll_hz;
       wheel_acc += -ry * wheel_rate / (double) poll_hz;
